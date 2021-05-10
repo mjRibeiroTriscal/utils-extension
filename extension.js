c5c3c34e66f76ff91085ebd2dc5cf3a33689d340
Mário Oliveira <mjribeiro@triscal.com.br>
2021-05-10 20:24:53 -0300

    cleaning extension.js

--- extension.js
+++ extension.js
@@ -27,56 +27,13 @@ const createFile = require('./src/utils/createFile/createFile')
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "utils-extension" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('utils-extension.createFile', function () {
-		
-		const htmlContent = `<!DOCTYPE html>
-<html lang="en">
-	<head>
-		<meta charset="UTF-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title>Document</title>
-	</head>
-	<body>
-		Content Here... ${new Date()}
-	</body>
-</html>`;
-
-		// console.log(vscode.workspace.workspaceFolders)
-
-		const folderPath = vscode.workspace.workspaceFolders[0].uri.path.toString().split(":")[1];
-
-		// fs.writeFile(path.join(folderPath, "index.html"), htmlContent, err => {
-		// 	if (err) {
-		// 		console.log(err)
-		// 		return vscode.window.showErrorMessage('Failed to create BoilerPlate HTML file!');
-		// 	}
-		// 	vscode.window.showInformationMessage('Created BoilerPlate HTML file!');
-		// })
-
-		createFile.default()		
-	});
+	console.log('Running "utils-extension"...');
 	
-	let disposable2 = vscode.commands.registerCommand('utils-extension.createFolder', function () {
-		// const root = vscode.workspace.workspaceFolders[0].uri.path.toString().split(":")[1];
-		// vscode.window.showInputBox().then(text => {
-		// 	if (!fs.existsSync(`${root}/${text}`)) {
-		// 		fs.mkdirSync(`${root}/${text}`)
-		// 	}
-		// })
-		createFolder.default()
-	});
+	let _createFile = vscode.commands.registerCommand('utils-extension.createFile', () => createFile.default())
+	let _createFolder = vscode.commands.registerCommand('utils-extension.createFolder', () => createFolder.default())
 
-	context.subscriptions.push(disposable);
-	context.subscriptions.push(disposable2);
+	context.subscriptions.push(_createFile);
+	context.subscriptions.push(_createFolder);
 }
 
 // this method is called when your extension is deactivated
